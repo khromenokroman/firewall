5bffcba44a4f3e526893f9718960e70f60f5ef15
khromenokroman <roma55592@yandex.ru>
2025-09-16 19:57:25 +0200

    Added IP packet parsing at layer 3 and refined ether_type processing

--- src/interface.cpp
+++ src/interface.cpp
@@ -21,15 +21,10 @@ void packet_handler(u_char *, pcap_pkthdr const *pkthdr, u_char const *packet) {
     ::fmt::print("===== INFO PACKET L2 =====\n", pkthdr->len);
     ::fmt::print("Packet bytes: {}\n", pkthdr->len);
 
-    auto l2_header = (ether_header *) packet;
-    uint16_t ether_type = be16toh(l2_header->ether_type);
+    auto const l2_header = reinterpret_cast<ether_header const *>(packet);
 
-    if (ether_type == ETHERTYPE_IP) {
-        ::fmt::print("(IP) ether_type: {:#06x}\n", ether_type);
-    }
-    if (ether_type == ETHERTYPE_ARP) {
-        ::fmt::print("(ARP) ether_type: {:#06x}\n", ether_type);
-    }
+    uint16_t ether_type = be16toh(l2_header->ether_type);
+    ::fmt::print("ether_type: {:#06x}\n", ether_type);
 
     std::array<uint8_t, 6> mac_src{};
     memcpy(mac_src.data(), l2_header->ether_shost, sizeof l2_header->ether_shost);
@@ -55,6 +50,34 @@ void packet_handler(u_char *, pcap_pkthdr const *pkthdr, u_char const *packet) {
 
     ::fmt::print("===== END L2 =====\n");
 
+
+    if (ether_type == ETHERTYPE_IP) {
+        ::fmt::print("===== INFO PACKET L3 =====\n", pkthdr->len);
+
+        auto const ip_header = reinterpret_cast<ip const *>(packet + sizeof(ether_header));
+        ::fmt::print("Version: {}\n", ip_header->ip_v);
+        ::fmt::print("IP header length: {}\n", ip_header->ip_hl * 4);
+        ::fmt::print("Type of service: {}\n", ip_header->ip_tos);
+        ::fmt::print("Total length: {}\n", be16toh(ip_header->ip_len));
+        ::fmt::print("Identification: {}\n", be16toh(ip_header->ip_id));
+        ::fmt::print("TTL: {}\n", ip_header->ip_ttl);
+        ::fmt::print("Protocol: {}\n", ip_header->ip_p);
+        ::fmt::print("Header checksum: {}\n", be16toh(ip_header->ip_sum));
+
+        auto fragment_field = be16toh(ip_header->ip_off);
+        bool reserved_flag = (fragment_field & IP_RF) != 0;
+        bool dont_fragment_flag = (fragment_field & IP_DF) != 0;
+        bool more_fragments_flag = (fragment_field & IP_MF) != 0;
+        ::fmt::print("RF[{}] DF[{}] MF[{}]\n", reserved_flag, dont_fragment_flag, more_fragments_flag);
+
+        ::fmt::print("IP SRC: {}\n", inet_ntoa(ip_header->ip_src));
+        ::fmt::print("IP DST: {}\n", inet_ntoa(ip_header->ip_dst));
+
+        ::fmt::print("===== END L3 =====\n");
+    }
+    if (ether_type == ETHERTYPE_ARP) {
+        ::fmt::print("(ARP) ether_type: {:#06x}\n", ether_type);
+    }
 }
 
 void Interface::read() { pcap_loop(m_raw_interface.get(), 0, packet_handler, NULL); }
